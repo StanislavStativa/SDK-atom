fddd7b6805e7119bfe379362b5cf98df138f74eb
Stanislav Stativa <stas.stativa@gmail.com>
2026-04-24 10:21:50 +0300

    2-0-0

--- examples/kit-nextjs-skate-park/src/byoc/index.tsx
+++ examples/kit-nextjs-skate-park/src/byoc/index.tsx
@@ -1,8 +1,6 @@
-'use client';
-
 import React, { JSX } from 'react';
 import * as FEAAS from '@sitecore-feaas/clientside/react';
-import * as Events from '@sitecore-content-sdk/events/browser';
+import * as Events from '@sitecore-content-sdk/events';
 import { LayoutServicePageState, SitecoreProviderReactContext } from '@sitecore-content-sdk/nextjs';
 import '@sitecore/components/context';
 import dynamic from 'next/dynamic';
